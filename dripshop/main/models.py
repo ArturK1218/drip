416c01a1193155d74b475aa4eaca87641d6a3e68
dripshop <dripshop@.com>
2025-06-23 21:34:31 +0300

    добавлена фильтрация

--- dripshop/main/models.py
+++ dripshop/main/models.py
@@ -33,6 +33,7 @@ class ClothingItem(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE,
                                     related_name='clothing_item')
     description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='product/%Y/%m/%d', blank=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True)
     price = models.DecimalField(max_digits=20, decimal_places=2)
--- dripshop/main/views.py
+++ dripshop/main/views.py
@@ -8,7 +8,7 @@ class CatalogView(ListView):
     template_name = 'main/product/list.html'
     context_object_name = 'clothing_items'
 
-    def def get_queryset(self):
+    def get_queryset(self):
         queryset = super().get_queryset()
         category_slugs = self.request.GET.getlist('category')
         size_names = self.request.GET.getlist('size')
@@ -18,10 +18,11 @@ class CatalogView(ListView):
         if category_slugs:
             queryset = queryset.filter(category__slug__in=category_slugs)
 
+        
         if size_names:
             queryset = queryset.filter(
                 Q(sizes__name__in=size_names) & Q(sizes__clothingitemsize__available=True)
-            )
+            ).distinct()
         
         if min_price:
             queryset = queryset.filter(price__gte=min_price)
@@ -34,8 +35,8 @@ class CatalogView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['categories'] = Category.obects.all()
-        context['sizes'] = Sizes.obects.all()
+        context['categories'] = Category.objects.all()
+        context['sizes'] = Size.objects.all()
         context['selected_categories'] = self.request.GET.getlist('category')
         context['selected_sizes'] = self.request.GET.getlist('size')
         context['min_price'] = self.request.GET.get('min_price', '')
